8c35371ab3419d0167680755788ab5f7a4aea0c0
Francois Gleyze <fgleyze[at]gmail.com>
2020-08-26 16:24:40 +0200

    Display a basic mesh with Orbit Control

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react'
 import './App.css';
+import { extend, Canvas, useFrame, useThree } from 'react-three-fiber';
+import { OrbitControls } from "three-orbitcontrols";
+
+extend({ OrbitControls });
+
+function Controls(props) {
+  const controls = useRef()
+  const { scene, camera, gl } = useThree()
+  useFrame(() => controls.current.update())
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} />
+}
+
+function Box(props) {
+  const mesh = useRef()
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+    >
+      <boxBufferGeometry attach="geometry" args={[5, 3, 1]} />
+      <meshStandardMaterial attach="material" color='orange' />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%', height: '100%', position: 'absolute' }}>
+      <Canvas camera={{ position: [5, 5, 10] }}>
+        <Controls />
+        <pointLight position={[10, 7, 5]} />
+        <pointLight position={[-10, -7, -5]} />
+        <Box position={[0, 0, 0]} />
+      </Canvas>
     </div>
   );
 }
