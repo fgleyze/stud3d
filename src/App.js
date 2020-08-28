9e4f7ae8a7f6a1e84eee9cb08625f95e48e2253f
Francois Gleyze <fgleyze[at]gmail.com>
2020-08-28 21:26:13 +0200

    Decouple studs data from rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo } from 'react'
+import React, { useRef, useState, useMemo } from 'react'
 import './App.css';
 import { extend, Canvas, useFrame, useThree } from 'react-three-fiber';
 import { OrbitControls } from "three-orbitcontrols";
@@ -19,6 +19,7 @@ function Controls(props) {
 
 function Stud(props) {
   const mesh = useRef()
+  const [hovered, setHover] = useState(false)
 
   let positions = [];
   const resetPositions = props.dimensions.map(coor => coor / 2)
@@ -42,38 +43,24 @@ function Stud(props) {
         <edgesGeometry attach="geometry" args={[geom]} />
         <lineBasicMaterial color="black" attach="material" />
       </lineSegments>
-      <mesh ref={mesh}>
+      <mesh
+        ref={mesh}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
         <boxBufferGeometry attach="geometry" args={props.dimensions.map(coor => coor / 100)} />
-        <meshStandardMaterial attach="material" color='orange' />
+        <meshStandardMaterial attach="material" color={hovered ? 'red' : 'orange'} />
       </mesh>
     </group>
   )
 }
 
-function CommonStud(props) {
-  return (
-    <Stud dimensions={[45, props.height - (3 * 45), 145]} positions={[props.offset, 45, 0]}/>
-  );
-}
-
-class Canvas3d extends React.Component {
-  renderCommonStud(offset, height) {
-    return (
-      <CommonStud key={offset} offset={offset} height={height} />
-    );
-  }
-  
+class Canvas3d extends React.Component {  
   render() {
     const length = this.props.length;
     const height = this.props.height;
 
     const wallDimensions = [length, height, 145];
-
-    let commonStuds = []
-
-    for ( let offset = 0; offset < (this.props.rightJunction == femaleCornerJunction ? length - (145 + 45 * 2) : length); offset += 645 ) {
-      commonStuds.push(this.renderCommonStud(offset, height))
-    }
 
     const doubleTopPlate = {
       length: length - 2 * 145,
@@ -91,30 +78,67 @@ class Canvas3d extends React.Component {
       doubleTopPlate.xPosition = 145;
     }
 
+    let studs = [
+      // bottom plate
+      {
+        dimensions: [length, 45, 145],
+        positions: [0, 0, 0],
+      },
+      // top plate
+      {
+        dimensions: [length, 45, 145],
+        positions: [0, height - (2 * 45), 0],
+      },
+      // double top plate
+      {
+        dimensions: [doubleTopPlate.length, 45, 145],
+        positions: [doubleTopPlate.xPosition, height - 45, 0],
+      },
+      // last common stud
+      {
+        dimensions: [45, height - (3 * 45), 145],
+        positions: [length - 45, 45, 0],
+      },
+    ];
+    
+    // common studs
+    for ( let offset = 0; offset < (this.props.rightJunction == femaleCornerJunction ? length - (145 + 45 * 2) : length); offset += 645 ) {
+      studs.push({
+        dimensions: [45, height - (3 * 45), 145],
+        positions: [offset, 45, 0]
+      })
+    }
+
+    // left corner female junction stud
+    if (this.props.leftJunction == femaleCornerJunction) {
+      studs.push({
+        dimensions: [145, height - (3 * 45), 45],
+        positions: [45, 45, 100]
+      });
+    }
+
+    // right corner female junction stud
+    if (this.props.rightJunction == femaleCornerJunction) {
+      studs.push({
+        dimensions: [145, height - (3 * 45), 45],
+        positions: [length - (45 + 145), 45, 100]
+      });
+    }
+
+    let renderedStuds = [];
+
+    for (let stud of studs) {
+      renderedStuds.push(<Stud dimensions={stud.dimensions} positions={stud.positions}/>)
+    }
+
     return <Canvas camera={{ position: [-12, 0, 30] }}>
       <Controls />
       <axesHelper/>
       <pointLight position={[150, 140, 100]} />
       <pointLight position={[-150, -140, -100]} />
       <group position={wallDimensions.map(coor => -Math.abs(coor / 100 / 2))}>
-        // bottom plate
-        <Stud dimensions={[length, 45, 145]} positions={[0, 0, 0]}/>
-        // top plate
-        <Stud dimensions={[length, 45, 145]} positions={[0, height - (2 * 45), 0]}/>
-        // double top plate
-        <Stud dimensions={[doubleTopPlate.length, 45, 145]} positions={[doubleTopPlate.xPosition, height - 45, 0]}/>
-
-        // common studs
-        {commonStuds}
-
-        // last common stud
-        <Stud dimensions={[45, height - (3 * 45), 145]} positions={[length - 45, 45, 0]}/>
-
-        //left male corner junction
-        {this.props.leftJunction == femaleCornerJunction && <Stud dimensions={[145, height - (3 * 45), 45]} positions={[45, 45, 100]}/>}
-
-        //right male corner junction
-        {this.props.rightJunction == femaleCornerJunction && <Stud dimensions={[145, height - (3 * 45), 45]} positions={[length - (45 + 145), 45, 100]}/>}
+        //studs
+        {renderedStuds}
       </group>
     </Canvas>
   }
@@ -126,6 +150,7 @@ class App extends React.Component {
     height: 2500,
     leftJunction: femaleStraigthJunction,
     rightJunction: femaleStraigthJunction,
+    studs: [],
   };
 
   handleLengthChange = event => {
@@ -144,12 +169,16 @@ class App extends React.Component {
     this.setState({ rightJunction: event.target.value });
   };
 
+  createStuds() {
+
+  }
+
   render() {
     return (
       <div className="flex">
         <div className="relative flex-none border-r border-solid border-gray-300">
           <div className="px-4">
-            <p className="py-4 text-center">Stud 3D</p>
+            <p className="py-4 text-center text-xl">Stud 3D</p>
 
             <label className="block mb-1">Longueur en mm :</label>
             <input
