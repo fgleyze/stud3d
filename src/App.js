70f13b2adedffbe500b88b98bee91fb047e214bb
Francois Gleyze <fgleyze@gmail.com>
2020-09-02 14:08:21 +0200

    Can add an opening to wall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useMemo } from 'react'
 import './App.css';
-import { extend, Canvas, useFrame, useThree } from 'react-three-fiber';
+import { extend, Canvas, useFrame, useThree, createPortal } from 'react-three-fiber';
 import { OrbitControls } from "three-orbitcontrols";
 import { BoxBufferGeometry } from 'three';
 
@@ -10,6 +10,11 @@ const junctions = {
   femaleStraigth: 0,
   maleStraigth: 1,
   femaleCorner: 2,
+}
+
+const studTypes = {
+  commonStud: "commonStud",
+  other: "other",
 }
 
 function Controls(props) {
@@ -78,7 +83,7 @@ function Canvas3d(props) {
     </Canvas>
 }
 
-function Form(props) {
+function WallForm(props) {
   function renderSelect(name, value, handleWallChange) {
     return   <select
       name={name}
@@ -93,6 +98,8 @@ function Form(props) {
   }
 
   return <div>
+    <h1 className="text-lg mb-2">Mur</h1>
+
     <label className="block mb-1">Longueur en mm :</label>
     <input
       className="block w-full mb-4 border rounded py-2 px-3"
@@ -119,6 +126,48 @@ function Form(props) {
   </div>
 }
 
+function OpeningForm(props) {
+  return <div>
+    <h1 className="text-lg mb-2">Ouverture</h1>
+
+    <label className="block mb-1">Largeur en mm :</label>
+    <input
+      className="block w-full border rounded py-2 px-3"
+      type="number"
+      name="length"
+      value={props.opening.length}
+      onChange={props.handleOpeningChange}
+    />
+
+    <label className="block mb-1">Hauteur en mm :</label>
+    <input
+      className="block w-full mb-4 border rounded py-2 px-3"
+      type="number"
+      name="height"
+      value={props.opening.height}
+      onChange={props.handleOpeningChange}
+    />
+
+    <label className="block mb-1">Décalage en mm :</label>
+    <input
+      className="block w-full mb-4 border rounded py-2 px-3"
+      type="number"
+      name="leftDistance"
+      value={props.opening.leftDistance}
+      onChange={props.handleOpeningChange}
+    />
+
+    <label className="block mb-1">Hauteur d'allège en mm :</label>
+    <input
+      className="block w-full mb-4 border rounded py-2 px-3"
+      type="number"
+      name="sill"
+      value={props.opening.sill}
+      onChange={props.handleOpeningChange}
+    />
+  </div>
+}
+
 function StudSections(props) {
   let sections = [];
 
@@ -139,22 +188,56 @@ class App extends React.Component {
       leftJunction: junctions.femaleStraigth,
       rightJunction: junctions.femaleStraigth,
     },
+    opening: {
+      leftDistance: 680,
+      length: 800,
+      height: 950,
+      sill: 500,
+    },
     studs: [],
   };
 
   componentDidMount() {
+    let studs = this.calculateOpeningStuds(
+      this.calculateWallStuds(this.state.wall), 
+      this.state.opening, 
+      this.state.wall
+    );
+
     this.setState({ 
-      studs: this.calculateWallStuds(this.state.wall)
+      studs,
     });
  }
 
   handleWallChange = event => {
     const wall = Object.assign({}, this.state.wall);
-    wall[event.target.name] = event.target.value;
+    wall[event.target.name] = parseInt(event.target.value);
+
+    let studs = this.calculateOpeningStuds(
+      this.calculateWallStuds(wall), 
+      this.state.opening, 
+      wall
+    );
 
     this.setState({ 
       wall,
-      studs: this.calculateWallStuds(wall)
+      studs
+    });
+  };
+
+  handleOpeningChange = event => {
+    const opening = Object.assign({}, this.state.opening);
+    opening[event.target.name] = parseInt(event.target.value);
+
+    let studs = this.calculateOpeningStuds(
+      this.calculateWallStuds(this.state.wall), 
+      opening, 
+      this.state.wall
+    );
+
+    this.setState({ 
+      opening,
+      studs
     });
   };
 
@@ -169,7 +252,7 @@ class App extends React.Component {
       positionX: 145,
     }
 
-    // calculate double top plate lenght
+    // calculate double top plate length
     if (leftJunction == junctions.maleStraigth && rightJunction == junctions.maleStraigth) {
       doubleTopPlate.length = length + 2 * 145;
       doubleTopPlate.positionX = -145;
@@ -186,21 +269,25 @@ class App extends React.Component {
       {
         dimensions: [length, 45, 145],
         positions: [0, 0, 0],
+        type: studTypes.other,
       },
       // top plate
       {
         dimensions: [length, 45, 145],
         positions: [0, height - (2 * 45), 0],
+        type: studTypes.other,
       },
       // double top plate
       {
         dimensions: [doubleTopPlate.length, 45, 145],
         positions: [doubleTopPlate.positionX, height - 45, 0],
+        type: studTypes.other,
       },
       // last common stud
       {
         dimensions: [45, height - (3 * 45), 145],
         positions: [length - 45, 45, 0],
+        type: studTypes.other,
       },
     ];
     
@@ -208,7 +295,8 @@ class App extends React.Component {
     for ( let offset = 0; offset < (rightJunction == junctions.femaleCorner ? length - (145 + 45 * 2) : length); offset += 645 ) {
       studs.push({
         dimensions: [45, height - (3 * 45), 145],
-        positions: [offset, 45, 0]
+        positions: [offset, 45, 0],
+        type: studTypes.commonStud,
       })
     }
 
@@ -216,7 +304,8 @@ class App extends React.Component {
     if (leftJunction == junctions.femaleCorner) {
       studs.push({
         dimensions: [145, height - (3 * 45), 45],
-        positions: [45, 45, 100]
+        positions: [45, 45, 100],
+        type: studTypes.other,
       });
     }
 
@@ -224,10 +313,131 @@ class App extends React.Component {
     if (rightJunction == junctions.femaleCorner) {
       studs.push({
         dimensions: [145, height - (3 * 45), 45],
-        positions: [length - (45 + 145), 45, 100]
+        positions: [length - (45 + 145), 45, 100],
+        type: studTypes.other,
       });
     }
 
+    return studs;
+  }
+
+  calculateOpeningStuds(studs, opening, wall) {
+    const wallHeight = wall.height ;
+    const leftDistance = opening.leftDistance;
+    const length = opening.length;
+    const height = opening.height;
+    const sill = opening.sill;
+
+    studs.push(...[
+      // left king stud
+      {
+        dimensions: [45, wallHeight - (3 * 45), 145],
+        positions: [leftDistance - (45 * 2), 45, 0],
+        type: studTypes.other,
+      },
+      // right king stud
+      {
+        dimensions: [45, wallHeight - (3 * 45), 145],
+        positions: [leftDistance + length + 45, 45, 0],
+        type: studTypes.other,
+      },
+      // header 1
+      {
+        dimensions: [length + (45 * 2), 145, 45],
+        positions: [leftDistance - 45, sill + height, 0],
+        type: studTypes.other,
+      },
+      // header 2
+      {
+        dimensions: [length + (45 * 2), 145, 45],
+        positions: [leftDistance - 45, sill + height, 45],
+        type: studTypes.other,
+      },
+      // header 3
+      {
+        dimensions: [length + (45 * 2), 145, 45],
+        positions: [leftDistance - 45, sill + height, 45 * 2],
+        type: studTypes.other,
+      },
+      // left trimmer stud
+      {
+        dimensions: [45, height, 145],
+        positions: [leftDistance - 45, sill, 0],
+        type: studTypes.other,
+      },
+      // left top trimmer stud
+      {
+        dimensions: [45, wallHeight - (sill + height + (2 * 45) + 145), 145],
+        positions: [leftDistance - 45, sill + height + 145, 0],
+        type: studTypes.other,
+      },
+      // right trimmer stud
+      {
+        dimensions: [45, height, 145],
+        positions: [leftDistance + length, sill, 0],
+        type: studTypes.other,
+      },
+      // right top trimmer stud
+      {
+        dimensions: [45, wallHeight - (sill + height + (2 * 45) + 145), 145],
+        positions: [leftDistance + length, sill + height + 145, 0],
+        type: studTypes.other,
+      },
+    ]);
+
+    // sill
+    if (sill > 45 && sill < (2 * 45)) {
+      studs.push({
+        dimensions: [length + (45 * 2), sill, 145],
+        positions: [leftDistance - 45, sill - 45, 0],
+        type: studTypes.other,
+      })
+    } else if (sill > (2 * 45)) {
+      studs.push({
+        dimensions: [length + (45 * 2), 45, 145],
+        positions: [leftDistance - 45, sill - 45, 0],
+        type: studTypes.other,
+      })
+    }
+
+    if (sill > 45) {
+      // left lower trimmer stud
+      studs.push(...[{
+        dimensions: [45, sill - (2 * 45), 145],
+        positions: [leftDistance - 45, 45, 0],
+        type: studTypes.other,
+      },
+      // right lower trimmer stud
+      {
+        dimensions: [45, sill - (2 * 45), 145],
+        positions: [leftDistance + length, 45, 0],
+        type: studTypes.other,
+      }])
+    }
+
+    // remove or replace overlapping commn studs
+    // https://stackoverflow.com/questions/24812930/how-to-remove-element-from-array-in-foreach-loop
+    studs.reduceRight(function (acc, stud, key, object) {
+      if (stud.type === studTypes.commonStud && stud.positions[0] > leftDistance - (3 * 45) && stud.positions[0] < leftDistance
+      || stud.type === studTypes.commonStud && stud.positions[0] > (leftDistance + length) - (45) && stud.positions[0] < (leftDistance + length) + (2 * 45)) {
+        object.splice(key, 1);
+      } else if (stud.type === studTypes.commonStud && stud.positions[0] >= leftDistance - (2 * 45) && stud.positions[0] <= (leftDistance + length + 45)) {
+        //top jack studs
+        let topJackStud = JSON.parse(JSON.stringify(object[key]));
+        topJackStud.dimensions[1] = wallHeight - (sill + height + 145 + (2 * 45));
+        topJackStud.positions[1] = sill + height + 145;
+        object.push(topJackStud);
+
+        if (sill <= 45) {
+          object.splice(key, 1);
+        } else {
+          //bottom jack studs
+          let bottomJackStud = JSON.parse(JSON.stringify(object[key]));
+          bottomJackStud.dimensions[1] = sill - (2 * 45);
+          object[key] = bottomJackStud;
+        }
+      }
+    });
     return studs;
   }
 
@@ -237,9 +447,13 @@ class App extends React.Component {
         <div className="relative flex-none border-r border-solid border-gray-300">
           <div className="px-4">
             <p className="py-4 text-center text-xl">Stud 3D</p>
-            <Form 
+            <WallForm 
               wall={this.state.wall}
               handleWallChange={this.handleWallChange}
+            />
+            <OpeningForm 
+              opening={this.state.opening}
+              handleOpeningChange={this.handleOpeningChange}
             />
           </div>
           <a href="https://github.com/fgleyze/stud3d" target="_blank" className="w-full absolute bottom-0 pb-4">
